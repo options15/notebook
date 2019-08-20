6d2cfc9c0bb978985206a71a29afebe24804173c
options15 <options.max@mail.ru>
2019-08-21 03:00:28 +0400

    Create validation to input parameters

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Notebook.Data.Interfaces;
 using Notebook.Data.Models;
+using Notebook.Data.ValidateModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,30 +19,43 @@ namespace Notebook.Controllers
 
         public ViewResult Index()
         {
+            Validate val = new Validate();
+            ViewBag.NotValid = val;
             return View(_persons.AllPersons);
         }
         [HttpPost]
         [Route("Person/Create")]
         public ViewResult Index(string surname, string name, string phoneNamber, int yearOfBirth)
         {
-                    _persons.AddPerson(new Person(surname, name, phoneNamber, yearOfBirth));
-                    return View(_persons.AllPersons);
+            Validate val = new Validate();
+            val.ValidatePerson(surname, name, phoneNamber, yearOfBirth);
+            if (val.valid)
+                _persons.AddPerson(new Person(surname, name, phoneNamber, yearOfBirth));
+            else
+                ViewBag.NotValid = val;
+            return View(_persons.AllPersons);
         }
         [Route("Person/Delete/{Id}")]
         public ViewResult Index(int Id)
         {
+            Validate val = new Validate();
+            ViewBag.NotValid = val;
             _persons.DeletePerson(Id);
             return View(_persons.AllPersons);
         }
         [Route("Person/Search")]
         public ViewResult Index(string name, string surname, string phoneNamber)
         {
+            Validate val = new Validate();
+            ViewBag.NotValid = val;
             _persons.Search(name, surname, phoneNamber);
             return View(_persons.FoundPerson);
         }
         [Route("Person/Sort")]
         public ViewResult Index(string sortValue)
         {
+            Validate val = new Validate();
+            ViewBag.NotValid = val;
             _persons.SortPerson(sortValue);
             return View(_persons.SortedPerson);
         }
---
+++ Data/ValidateModel/Validate.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notebook.Data.ValidateModel
+{
+    public class Validate
+    {
+        public bool valid;
+        public string Surname{ get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string YearOfBirth { get; set; }
+
+        string NVName = "Please enter a valid name";
+        string NVSurname = "Please enter a valid surname";
+        string NVPhone = "Please enter a valid phone number";
+        string NVYear = "Please enter a valid year";
+        public Validate()
+        {
+            valid = false;
+            Surname = "";
+            Name = "";
+            PhoneNumber = "";
+            YearOfBirth = "";
+        }
+        public void ValidatePerson(string surname, string name, string phoneNumber, int yearOfBirth)
+        {
+            if (!CheckNameAndSurname(surname))
+            { Surname = NVSurname; }
+            if (!CheckNameAndSurname(name))
+            { Name = NVName; }
+            if (!ChecPhoneNumber(phoneNumber))
+            { PhoneNumber = NVPhone; }
+            if (!CheckYearOfBirth(yearOfBirth))
+            { YearOfBirth = NVYear; }
+            if (CheckNameAndSurname(surname) && CheckNameAndSurname(name) && ChecPhoneNumber(phoneNumber) && CheckYearOfBirth(yearOfBirth))
+            {
+                valid = true;
+            }
+            else
+            {
+                valid = false;
+            }
+        }
+
+        private bool ChecPhoneNumber(string phoneNumber)
+        {
+            if (phoneNumber == null)
+                return false;
+            for (int i = 0; i < phoneNumber.Length; i++)
+            {
+                if (!char.IsDigit(phoneNumber[i]))
+                    return false;
+            }
+            return true;
+        }
+
+        private bool CheckYearOfBirth(int yearOfBirth)
+        {
+            if (yearOfBirth < 1900 && yearOfBirth > 2010)
+                return false;
+            return true;
+        }
+
+        private bool CheckNameAndSurname(string str)
+        {
+            if (str == null)
+                return false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                    return false;
+            }
+                return true;
+        }
+    }
+}
